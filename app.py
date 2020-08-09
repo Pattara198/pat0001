e65c682b243c71cb2e19d51639035f761b822bac
Pattara198 <61036043+Pattara198@users.noreply.github.com>
2020-08-09 15:46:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import keras
 from flask import Flask, render_template, session, redirect, url_for, session
 from flask_wtf import FlaskForm
 from wtforms import TextField,SubmitField
 from wtforms.validators import NumberRange
 
 import numpy as np  
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 import joblib
 
 
